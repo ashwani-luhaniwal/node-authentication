4206ac615c4b880eef508c4d1d07fc517c93976a
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-07-04 07:20:53 -0700

    User mongoose schema model

---
+++ node-mongodb-authentication/models/user.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+let UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  passwordConf: {
+    type: String,
+    required: true
+  }
+});
+
+// authenticate input against database
+UserSchema.statics.authenticate = function (email, password, callback) {
+  User.findOne({ email: email })
+    .exec((err, user) => {
+      if (err) {
+        return callback(err);
+      } else if (!user) {
+        let err = new Error('User not found.');
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      })
+    });
+}
+
+// hashing a password before saving it to database
+UserSchema.pre('save', function (next) {
+  let user = this;
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
+let User = mongoose.model('User', UserSchema);
+module.exports = User;
